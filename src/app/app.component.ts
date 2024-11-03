4543e27230d9645a539508b9c40ac3e73cec00fb
Emanuel Sorin <emyemy11899@gmail.com>
2024-11-03 16:08:23 +0100

    desplegar aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 @Component({
   selector: 'app-root',
@@ -8,3 +12,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Japo';
 }
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtcPrgwyuMEG7KE1ZOCvuGOyAhLggxh-o",
+  authDomain: "kaado-f0e50.firebaseapp.com",
+  projectId: "kaado-f0e50",
+  storageBucket: "kaado-f0e50.firebasestorage.app",
+  messagingSenderId: "822862642234",
+  appId: "1:822862642234:web:8ee81c34a8205e1e474903"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
